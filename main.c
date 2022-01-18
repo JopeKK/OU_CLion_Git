15104473befff1696ed261c00c785102d0ed9aa3
JopeKK <w.j.jopkiewicz@gmail.com>
2022-01-18 18:40:41 +0100

    Zmiany dodatkowe w 03

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+int Policz(int a,int b)
+{
+    return (a+b)*(a-b);
+}
 int main() {
-    printf("Hello, World!\n");
+    printf("W 3 branchu (2+3)*(2-3)=%d", Policz(2,3));
     return 0;
 }
